7a6cd4ad830b697e9afc0a3009542f1a7254ae0e
rasc3ta <greenfair.shan5@gmail.com>
2024-10-27 15:52:15 +0600

    added login and register functionality

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,28 @@
 "use client";
 import LoginForm from "@/app/components/LoginForm";
 import Registration from "@/app/components/Registration";
-// import { useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import React from "react";
 
 const Home = () => {
-  // const session = useSession();
+  const { status } = useSession();
+  if (status !== "authenticated") {
+    return (
+      <div className="max-w-[500px] flex flex-col gap-3 items-center justify-center pt-10">
+        <LoginForm></LoginForm>
+        {console.log(status)}
+      </div>
+    );
+  }
+
   return (
-    <div className="max-w-[500px] flex flex-col gap-3 items-center justify-center pt-10">
-      <LoginForm></LoginForm>
-      {/* {console.log(session)} */}
+    <div>
+      todos
+      <div className="w-full flex items-center justify-center">
+        <button className="text-2xl border-[4px] border-black px-2 py-4">
+          Logout
+        </button>
+      </div>
     </div>
   );
 };
